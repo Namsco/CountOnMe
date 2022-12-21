616c9c2f376bf115cda64b6b31dc6bb5cfacdee1
Namsco <naim.zidouni@outlook.fr>
2022-12-21 16:09:36 +0100

    Added a Model named "SimpleCalc".

---
+++ Model/SimpleCalc.swift
@@ -0,0 +1,14 @@
+//
+//  SimpleCalc.swift
+//  CountOnMe
+//
+//  Created by Zidouni on 19/12/2022.
+//  Copyright © 2022 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class SimpleCalcl {
+    
+    
+}
