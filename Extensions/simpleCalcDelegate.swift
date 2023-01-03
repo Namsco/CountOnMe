45d37c9b3528051fa5fe0e4eb28c3124d70c8c1a
Namsco <naim.zidouni@outlook.fr>
2023-01-03 21:33:58 +0100

    Added comments.

--- Extensions/simpleCalcDelegate.swift
+++ Extensions/simpleCalcDelegate.swift
@@ -11,10 +11,12 @@ import UIKit
 
 extension ViewController: SimpleCalcDelegate {
     
+    // Function to receive model's textView and give his data to controller's textView.
     func didReceiveData(_ data: String) {
         textView.text = simpleCalcl.textView
     }
     
+    // Function who has the logic of UIAlertController. 
     func displayAlert(_ message: String) {
         let alertVC = UIAlertController(title: "Zéro!", message: message, preferredStyle: .alert)
         alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
