4a9e5ad2c914513aa9ebcfcd658b3d5d6a69844b
Namsco <naim.zidouni@outlook.fr>
2023-01-04 16:40:13 +0100

    Added comments and fix some issues.

--- Model/SimpleCalc.swift
+++ Model/SimpleCalc.swift
@@ -18,7 +18,7 @@ class SimpleCalc {
     
     // MARK: - Private variables
     var result: Double = 0
-    private var elementsIndex = 0
+    private var elementsIndex: Int!
     
     private var elements: [String] {
         return textView.split(separator: " ").map { "\($0)" }
@@ -40,13 +40,13 @@ class SimpleCalc {
         return elements.count >= 1
     }
     
-    private var textError: Bool {
-        return textView == "Error"
+    private var textIsError: Bool {
+        return textView != "Error"
     }
     
     // MARK: - Public variables
-    weak var delegate: SimpleCalcDelegate?
-    var textView = String()
+    weak var delegate: SimpleCalcDelegate? // For pass data to controller.
+    var textView = String() // For pass this textView data's to controller.
     
     // MARK: - Private functions
     func sendDataToController(data: String) {
@@ -57,6 +57,7 @@ class SimpleCalc {
         delegate?.displayAlert(message)
     }
     
+    // We created a divisionOperation function to control if the number is divide by 0.
     private func divisionOperation(left: Double, right: Double) -> Double{
         if left == 0 || right == 0 {
             textView = "Error"
@@ -71,9 +72,11 @@ class SimpleCalc {
     
     // MARK: - Public functions
     func addNumber(number: String) {
-        guard !textError else {return sendAlertToController(message: "Please start a new calcul !")}
-        if elements.count > 0 {
-            if elements[0 + elementsIndex].contains(".") && number == "." {
+        elementsIndex = elements.count - 1 // To control the dot when we have a decimal number, we must created pass a value for element's index, this is always equal to elements.count - 1.
+        guard textIsError else {return sendAlertToController(message: "Please start a new calcul !")}
+
+        if elements.count > 0 { // If we add an operator before a number, elements.count is > 0.
+            if elements[elementsIndex].contains(".") && number == "." { // We control if user add "." when the number is already a decimal number.
                 sendAlertToController(message: "You already enter a point !")
             } else {
                 textView += number
@@ -83,16 +86,13 @@ class SimpleCalc {
             textView += number
             sendDataToController(data: number)
         }
-        if number != "." && elements.count > 1{
-            elementsIndex += 1
-        }
     }
     
     func addOperator(_ symbol: String) {
-        guard !textError else {return sendAlertToController(message: "Please start a new calcul !")}
-        let spacingOperation = " " + symbol + " "
+        guard textIsError else {return sendAlertToController(message: "Please start a new calcul !")}
+        let spacingOperation = " " + symbol + " " // We add space to the operation for more visibility.
         
-        if symbol == "+" && textView == "" || symbol == "-" && textView == "" {
+        if symbol == "+" && textView == "" || symbol == "-" && textView == "" { // We can add "+" or "-" operator if textView is Empty.
             textView += spacingOperation
             return sendDataToController(data: symbol)
             
@@ -109,40 +109,39 @@ class SimpleCalc {
     }
     
     func clearTextView(){
-        guard !textView.isEmpty else {return}
-        elementsIndex = 0
+        guard !textView.isEmpty else {return sendAlertToController(message: "There are no content to remove !")}
         textView = ""
         sendDataToController(data: textView)
     }
     
     func clearLastCharacter() {
-        guard !textError else {return sendAlertToController(message: "Please start a new calcul !")}
-        if !textView.isEmpty {
+        guard textIsError else {return sendAlertToController(message: "Please start a new calcul !")}
+        if !textView.isEmpty { // We control if text is not empty, if it isn't, we can remove last character inside the textView.
             textView.removeLast()
             sendDataToController(data: textView)
         } else {
-            elementsIndex = 0
+            sendAlertToController(message: "There are no content to remove !")
         }
     }
     
     func calculate(){
         var operationsToReduce = elements
         
-        guard expressionIsCorrect else {return sendAlertToController(message: "Entrez une expression correcte !")}
-        guard expressionHaveEnoughElement else {return sendAlertToController(message: "Démarrez un nouveau calcul !")}
+        guard expressionIsCorrect else {return sendAlertToController(message: "Please enter a correct expression !")}
+        guard expressionHaveEnoughElement else {return sendAlertToController(message: "Please start a new calcul !")}
         
         // Iterate over operations while an operand still here
         while operationsToReduce.count > 1 {
-            var priority = 0
+            var priority = 0 // For control the priority inside the calcul.
             
-            if operationsToReduce[0] == "+" || operationsToReduce[0] == "-" {
+            if operationsToReduce[0] == "+" || operationsToReduce[0] == "-" { // If the first operator is equal to "+" or "-", we fusion it with the next number inside the array.
                 let newNumber = operationsToReduce[0] + operationsToReduce[1]
                 operationsToReduce[0] = newNumber
-                operationsToReduce.remove(at: 1)
+                operationsToReduce.remove(at: 1) // After the fusion we remove the element at the index 1.
             }
             
             if let index = operationsToReduce.firstIndex(where: {$0 == "x" || $0 == "÷"}) {
-                priority = index - 1
+                priority = index - 1 // We define priority's value as the index when we have multiplication or division operator - 1, to find index of the first number of the priority operation.
             }
             
             let left = Double(operationsToReduce[priority])!
@@ -158,20 +157,19 @@ class SimpleCalc {
             }
             
             for _ in 1...3 {
-                operationsToReduce.remove(at: priority)
+                operationsToReduce.remove(at: priority) // After the priority operation we remove numbers of this last.
             }
-            let newResult = result.removeZerosFromEnd()
-            var resultWithQuote = String()
+            let newResult = result.removeZerosFromEnd() // We create a new property to call the extension for remove .0 when it isn't following by an other number different from 0.
+            var resultWithDot = String()
             if newResult.contains(","){
-                resultWithQuote = newResult.replacingOccurrences(of: ",", with: ".")
+                resultWithDot = newResult.replacingOccurrences(of: ",", with: ".") // Inside the newResult we replace the "," to a ".".
             } else {
-                resultWithQuote = newResult
+                resultWithDot = newResult
             }
             
-            operationsToReduce.insert("\(resultWithQuote)", at: priority)
+            operationsToReduce.insert("\(resultWithDot)", at: priority)
             
-            if !textError {
-                elementsIndex = 0
+            if textIsError { // We control if the textView is equal at "Error"
                 textView = ""
                 textView.append("\(operationsToReduce.first!)")
                 sendDataToController(data: textView)
